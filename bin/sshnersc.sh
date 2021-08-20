e08ea951cfd31bba6271fbde7cac4bed781c7f9c
John Moustakas <john.moustakas@gmail.com>
2021-08-20 07:05:08 -0400

    update sshnersc script

--- bin/sshnersc.sh
+++ bin/sshnersc.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-# Copyright 2018 Regents of the University of California
 
-#
-# NOTICE.  This software was developed under funding from the U.S.
-# Department of Energy and the U.S. Government consequently retains
-# certain rights. As such, the U.S. Government has been granted for
-# itself and others acting on its behalf a paid-up, nonexclusive,
-# irrevocable, worldwide license in the software to reproduce,
-# distribute copies to the public, prepare derivative works, and
-# perform publicly and display publicly, and to permit other to do
-# so.
-
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL BERKELEY LAB OR THE U.S. GOVERNMENT BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# SSH Proxy (sshproxy), Copyright (c) 2019, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any required
+# approvals from the U.S. Dept. of Energy).  All rights reserved.
+# 
+# If you have questions about your rights to use or distribute this software,
+# please contact Berkeley Lab's Intellectual Property Office at  IPO@lbl.gov.
+# 
+# NOTICE.  This Software was developed under funding from the U.S. Department of Energy
+# and the U.S. Government consequently retains certain rights. As such, the U.S.
+# Government has been granted for itself and others acting on its behalf a paid-up,
+# nonexclusive, irrevocable, worldwide license in the Software to reproduce, distribute
+# copies to the public, prepare derivative works, and perform publicly and display
+# publicly, and to permit other to do so.
+# 
+# See LICENSE for full text.
 
 progname=$(basename $0)
 version="1.1.0"
