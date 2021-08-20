966de996a03ecf9dd7ea729bfd3b84f99bb16082
John Moustakas <john.moustakas@gmail.com>
2021-08-20 07:44:20 -0400

    updates for fall2021

--- teaching/siena_class_roster.py
+++ teaching/siena_class_roster.py
@@ -1,31 +1,30 @@
-import requests 
+import requests, pdb
 #import BeautifulSoup
 from bs4 import BeautifulSoup
-import HTMLParser
-from HTMLParser import HTMLParser
+#import HTMLParser
+#from HTMLParser import HTMLParser
 import sys
 import os
 
 # Open the file.
 r = open(sys.argv[1])
 
-if not os.path.isdir('Detail_Class_List_files'):
-    os.rename('Detail Class List_files', 'Detail_Class_List_files')
+if not os.path.isdir('Siena_Class_Roster_files'):
+    os.rename('Siena Class Roster_files', 'Siena_Class_Roster_files')
 
 # Try to parse the webpage by looking for the tables.
-soup = BeautifulSoup(r)
+soup = BeautifulSoup(r, "html.parser")
 
-print "\documentclass{article}"
-print "\usepackage{graphicx}"
-print "\usepackage{subfig}"
-
-print "\hoffset=-1.50in"
-print "\setlength{\\textwidth}{7.5in}"
-print "\setlength{\\textheight}{9in}"
-print "\setlength{\\voffset}{0pt}"
-print "\setlength{\\topmargin}{0pt}"
-print "\setlength{\headheight}{0pt}"
-print "\setlength{\headsep}{0pt}"
+print(r"\documentclass{article}")
+print(r"\usepackage{graphicx}")
+print(r"\usepackage{subfig}")
+print(r"\hoffset=-1.50in")
+print(r"\setlength{\textwidth}{7.5in}")
+print(r"\setlength{\textheight}{9in}")
+print(r"\setlength{\voffset}{0pt}")
+print(r"\setlength{\topmargin}{0pt}")
+print(r"\setlength{\headheight}{0pt}")
+print(r"\setlength{\headsep}{0pt}")
 
 
 h2s = soup.find_all('h2')
@@ -50,60 +49,64 @@ for table in tables:
             cols = row.findAll('td')
         
             for col in cols:
-                
                 img = col.findAll('img')
                 
-                a = col.findAll('a')
+                #a = col.findAll('a')
+                pp = row.findAll('p')
                 #if len(a)>0:
-                #    import pdb ; pdb.set_trace()
+                #    pdb.set_trace()
                 
-                if len(img)>0 and img[0]['src'].find('jpg')>=0:
+                if len(img)>0 and img[0]['src'].find('jpg') >= 0:
                     image = img[0]['src']
-                    image = image.replace(' ','_').replace('%20', '_')
+                    image = image.replace(' ', '_')
+                    #image = image.replace(' ','_').replace('%20', '_')
                     if not os.path.isfile(image):
+                        print('Missing image!')
                         import pdb ; pdb.set_trace()
                     #if os.path.isfile(image):
-                    #    import pdb ; pdb.set_trace()
+                    #    pdb.set_trace()
                         
-                if len(a)>0 and 'mailto' in a[0]['href']:
-                    name = a[0]['target']
-                #if len(a)>0 and a[0]['class']==['leftaligntext']:
-                #    name = a[0].string
-                #    print(image, a[0].string)
-                #import pdb ; pdb.set_trace()
+                #if len(a)>0 and 'mailto' in a[0]['href']:
+                #    name = a[0]['target']
+                if len(pp)>0 and pp[0]['class']==['leftaligntext']:
+                    name = pp[0].string
+                    #print(image, name)
+
+                #if 'Taryn' in name:
+                #    pdb.set_trace()
 
                 if name is not None and image is not None:
-                    if icount%25==0:
+                    if icount % 25 == 0:
                         if icount > 0:
-                            print "\\clearpage"
+                            print(r"\clearpage")
                         else:
-                            print "\\begin{document}"
-                        print "\\begin{figure}"
-                        print "\centering"
+                            print(r"\begin{document}")
+                        print(r"\begin{figure}")
+                        print(r"\centering")
                         closed_figure = False
 
                     if os.stat(image).st_size < 250:
                         #image = './file_not_found.jpg'
                         image = './smiley.png'
 
-                    if icount%5==4:
-                        print "\subfloat[%s]{\includegraphics[height=0.19\\textwidth]{%s}}\\\\" % (name,image)
+                    if icount % 5 == 4:
+                        print(r"\subfloat[%s]{\includegraphics[height=0.19\textwidth]{%s}}\\" % (name,image))
                     else:
-                        print "\subfloat[%s]{\includegraphics[height=0.19\\textwidth]{%s}}\\hfill" % (name,image)
+                        print(r"\subfloat[%s]{\includegraphics[height=0.19\textwidth]{%s}}\hfill" % (name,image))
 
                     image = None
                     name = None
 
-                    if icount%25==24:
-                        print "\caption{%s}" % (caption)
-                        print "\end{figure}"
+                    if icount % 25 == 24:
+                        print("\caption{%s}" % (caption))
+                        print("\end{figure}")
                         closed_figure = True
 
                     icount += 1
                     
 
 if not closed_figure:
-    print "\caption{%s}" % (caption)
-    print "\end{figure}"
-print "\end{document}"
+    print("\caption{%s}" % (caption))
+    print("\end{figure}")
+print("\end{document}")
 
